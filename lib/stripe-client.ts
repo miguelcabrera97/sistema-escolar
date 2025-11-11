b49a09e60ee7e26706b27e87097c32268c6d8e5e
miguelcabrera97 <migue122747@gmai.com>
2025-11-11 16:27:26 -0600

    sin stripe

--- lib/stripe-client.ts
+++
@@ -1,9 +0,0 @@
-import { loadStripe } from '@stripe/stripe-js'
-
-if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
-  throw new Error('NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY no está definida')
-}
-
-export const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-)

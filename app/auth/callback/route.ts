ca421f38262302a2dfbb4c43137f85e8e7394365
miguelcabrera97 <migue122747@gmai.com>
2026-02-16 11:43:21 -0600

    envia a la pagian de recuperar contraseña

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,40 +1,51 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
+import { type EmailOtpType } from '@supabase/supabase-js'
 
 export async function GET(request: Request) {
     const { searchParams, origin } = new URL(request.url)
     const code = searchParams.get('code')
-    // if "next" is in param, use it as the redirect URL
+    const token_hash = searchParams.get('token_hash')
+    const type = searchParams.get('type') as EmailOtpType | null
     const next = searchParams.get('next') ?? '/'
 
-    if (code) {
-        const cookieStore = await cookies()
-        const supabase = createServerClient(
-            process.env.NEXT_PUBLIC_SUPABASE_URL!,
-            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-            {
-                cookies: {
-                    get(name: string) {
-                        return cookieStore.get(name)?.value
-                    },
-                    set(name: string, value: string, options: any) {
-                        cookieStore.set({ name, value, ...options })
-                    },
-                    remove(name: string, options: any) {
-                        cookieStore.delete({ name, ...options })
-                    },
+    const cookieStore = await cookies()
+    const supabase = createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies: {
+                get(name: string) {
+                    return cookieStore.get(name)?.value
                 },
-            }
-        )
+                set(name: string, value: string, options: any) {
+                    cookieStore.set({ name, value, ...options })
+                },
+                remove(name: string, options: any) {
+                    cookieStore.delete({ name, ...options })
+                },
+            },
+        }
+    )
 
+    if (code) {
         const { error } = await supabase.auth.exchangeCodeForSession(code)
-
         if (!error) {
             return NextResponse.redirect(`${origin}${next}`)
         }
     }
 
-    // user is not logged in / error during exchange, redirect to error page or login
-    return NextResponse.redirect(`${origin}/login?error=auth-code-error`)
+    if (token_hash && type) {
+        const { error } = await supabase.auth.verifyOtp({
+            type,
+            token_hash,
+        })
+        if (!error) {
+            return NextResponse.redirect(`${origin}${next}`)
+        }
+    }
+
+    // return the user to an error page with some instructions
+    return NextResponse.redirect(`${origin}/login?error=auth-callback-error`)
 }
